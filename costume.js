8e3c47816a11eb21486e8f460ae29569be8d3bed
NicolasMillan <134756328+NicolasMillan@users.noreply.github.com>
2023-11-09 09:48:56 -0500

    Add files via upload

---
+++ costume.js
@@ -0,0 +1,17 @@
+// Declaring the variables for the pizza
+const h2SelectYourPizza = document.querySelector('h2');
+  const selectPizzaSize = document.querySelector('#pizza-size');
+  const pepperoniCheckbox = document.querySelector('#pepperoni');
+  const hamCheckbox = document.querySelector('#Ham');
+  const baconCheckbox = document.querySelector('#Bacon');
+  const mushroomsCheckbox = document.querySelector('#mushrooms');
+  const olivesCheckbox = document.querySelector('#olives');
+  const sausageCheckbox = document.querySelector('#sausage');
+  const thinCrustRadio = document.querySelector('#thin');
+  const panCrustRadio = document.querySelector('#pan');
+  const deepDishCrustRadio = document.querySelector('#deep-dish');
+  const h2DeliveryInformation = document.querySelectorAll('h2')[1];
+  const nameInput = document.querySelector('#name');
+  const addressInput = document.querySelector('#address');
+  const phoneInput = document.querySelector('#phone');
+  const orderButton = document.querySelector('input[type="submit"]');
